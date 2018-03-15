c6dd77c67855d9149b78ab1e0624949efc035dfe
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2018-03-15 20:15:40 -0300

    admin

--- app/tickets/admin.py
+++ app/tickets/admin.py
@@ -3,4 +3,22 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import State
+from .models import Ticket
+
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'state']
+    list_editable = ['state']
+    list_filter = ['state']
+    fields = ['title', 'description', 'state', 'created_at']
+    readonly_fields = ['created_at']
+    search_fields = ['title', 'description']
+
+
+admin.site.register(State, StateAdmin)
+admin.site.register(Ticket, TicketAdmin)

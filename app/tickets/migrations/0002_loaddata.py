5ce77285f2269c40394d15d78f61475df2869c37
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2018-03-15 21:52:19 -0300

    Datos iniciales para estados

---
+++ app/tickets/migrations/0002_loaddata.py
@@ -0,0 +1,17 @@
+from django.db import migrations
+from django.core.management import call_command
+
+
+def loadfixture(apps, schema_editor):
+    call_command('loaddata', 'initial_data.json')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tickets', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(loadfixture),
+    ]
